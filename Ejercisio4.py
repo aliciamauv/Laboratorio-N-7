d96565589a0dc645302f82ceaa091c3f6f4eb386
aliciamauv <68046658+aliciamauv@users.noreply.github.com>
2020-08-06 09:04:19 -0500

    Create Ejercisio4.py

---
+++ Ejercisio4.py
@@ -0,0 +1,10 @@
+mes=int(input("Mes: "))		
+
+if mes==1 or mes==3 or mes==5 or mes==7 or mes==8 or mes==10 or mes==12:
+    print ("Tiene 31 días")
+elif mes==4 or mes==6 or mes==9 or mes==11 :
+    print ("Tiene 30 días")
+elif mes==2:
+    print ("Tiene 28 o 29 días")
+else:
+    print ("Mes incorrecto")
